9f7d36b656e390e02a5f6bec8e1a2bd5095218be
Abhishekyadav3824 <aradaabhishek@gmail.com>
2025-09-17 10:02:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 const table = document.getElementById("sampleTable");
 
 function insert_Row() {
-    // Create a new row
-    const newRow = document.createElement("tr");
+    // Insert new row at position 0 (top of table)
+    const newRow = table.insertRow(0);
 
-    // Create two new cells
-    const cell1 = document.createElement("td");
-    const cell2 = document.createElement("td");
+    // Insert two cells
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
 
-    // Set their text
+    // Set text content
     cell1.innerText = "New Cell1";
     cell2.innerText = "New Cell2";
-
-    // Append cells into row
-    newRow.append(cell1, cell2);
-
-    // Insert row at the top of the table
-    table.insertBefore(newRow, table.firstChild);
 }
