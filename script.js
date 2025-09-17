fed7f741e455225c4770a9f55d518d85b583d209
Abhishekyadav3824 <aradaabhishek@gmail.com>
2025-09-17 09:51:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+const table=document.getElementById("sampleTable");
 function insert_Row() {
+	const tr=document.createElement("tr")
+	const td1=document.createElement("td")
+	const td2=document.createElement("td")
+	let count=1
+	td1.innerText=`New Cell${count}`
+	count++
+	td2.innerText=`New Cell${count}`
+	tr.append(td1,td2)
+	table.prepend(tr)
+	
     //Write your code here
   
   
